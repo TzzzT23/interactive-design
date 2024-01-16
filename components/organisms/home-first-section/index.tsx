68f57e27c12273d27d3f1a6cc230ff27e3871b92
TzzzT23 <tzc4tz@gmail.com>
2024-01-16 20:30:02 +0330

    ✨ Added last section with form validation (required only) with a welcome page

--- components/organisms/home-first-section/index.tsx
+++ components/organisms/home-first-section/index.tsx
@@ -15,52 +15,54 @@ export default function HomeFirstSection() {
   useTypewriterEffect()
 
   return (
-    <Col xxs={22} xl={20} className={styles['home']}>
-      <Header />
+    <Row justify='center' className={styles['home']} wrap>
+      <Col xxs={22} xl={20}>
+        <Header />
 
-      <Row
-        gutter={[8, 0]}
-        className={styles['home__content']}
-        justify='center'
-        wrap
-      >
-        <Col xxs={24} lg={12} data-selector='primary-content'>
-          <div className={styles['home__content--important-text']}>
-            Video Game Industry
-          </div>
-          <div className={styles['home__content--title']}>
-            Best Games of the Year
-          </div>
-          <Row
-            data-selector='typewriter'
-            className={styles['home__content--title']}
-          >
-            Gaming is{' '}
-            <span className={styles['home__content--typed-text']}></span>
-            <span className={styles['home__content--cursor']}>&nbsp;</span>
-          </Row>
-          <Chips
-            className={styles['home__content--chips']}
-            items={[
-              { title: 'Final Fantasy XVI' },
-              { title: 'Resident Evil 4' },
-              { title: "Marvel's Spider-Man 2" },
-              { title: 'Alan Wake 2' },
-              { title: "Baldur's Gate 3" },
-              { title: 'Super Mario Bros. Wonder' },
-            ]}
-          />
-          <Button
-            className={styles['home__content--action']}
-            icon={<ArrowRightSvg />}
-          >
-            View Nominees
-          </Button>
-        </Col>
-        <Col xxs={24} lg={10} className={styles['home__content--side']}>
-          <Image src='/assets/Illustration-1.png' alt='information' fill />
-        </Col>
-      </Row>
-    </Col>
+        <Row
+          gutter={[8, 0]}
+          className={styles['home__content']}
+          justify='center'
+          wrap
+        >
+          <Col xxs={24} lg={12} data-selector='primary-content'>
+            <div className={styles['home__content--important-text']}>
+              Video Game Industry
+            </div>
+            <div className={styles['home__content--title']}>
+              Best Games of the Year
+            </div>
+            <Row
+              data-selector='typewriter'
+              className={styles['home__content--title']}
+            >
+              Gaming is{' '}
+              <span className={styles['home__content--typed-text']}></span>
+              <span className={styles['home__content--cursor']}>&nbsp;</span>
+            </Row>
+            <Chips
+              className={styles['home__content--chips']}
+              items={[
+                { title: 'Final Fantasy XVI' },
+                { title: 'Resident Evil 4' },
+                { title: "Marvel's Spider-Man 2" },
+                { title: 'Alan Wake 2' },
+                { title: "Baldur's Gate 3" },
+                { title: 'Super Mario Bros. Wonder' },
+              ]}
+            />
+            <Button
+              className={styles['home__content--action']}
+              icon={<ArrowRightSvg />}
+            >
+              View Nominees
+            </Button>
+          </Col>
+          <Col xxs={24} lg={10} className={styles['home__content--side']}>
+            <Image src='/assets/Illustration-1.png' alt='information' fill />
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   )
 }
